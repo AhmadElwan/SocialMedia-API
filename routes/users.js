02005b183f527fd051069763c2e51506a8212221
Ahmad Elwan <ahmadmelwan02@gmail.com>
2024-01-02 19:51:16 +0300

    Completed users routes

--- routes/users.js
+++ routes/users.js
@@ -69,6 +69,73 @@ router.delete("/:id", async (req, res) => {
 }
 })
 
+// Get a user by his username
+
+router.get("/:username", async (req, res) => {
+    try{
+        const user = await User.findOne({username: req.params.username});
+        if (!user) {
+            return res.status(404).json("User not found");
+        }else{
+            const {password, updatedAt, ...other} = user._doc;
+            res.status(200).json(other);
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+// Follow a user
+
+router.put("/:id/follow", async (req, res) => {
+    if(req.body.userId !== req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user || !currentUser) { // Checks if both users exists 
+                return res.status(404).json("User not found");
+            }else {
+                if(!user.followers.includes(req.body.userId)){
+                    await user.updateOne({$push:{followers:req.body.userId}});
+                    await currentUser.updateOne({$push:{following:req.params.id}});
+                    res.status(200).json(`Started following ${user.username}`);
+                }else {
+                    res.status(403).json("You already follow this user !");
+                }
+        }}catch(err){
+            res.status(500).json(err);
+        }
+    }else {
+        res.status(403).json("You cannot follow yourself !!");
+    }
+})
+
+// Unfollow a user
+
+router.put("/:id/unfollow", async (req, res) => {
+    if(req.body.userId !== req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user || !currentUser) { // Checks if both users exists 
+                return res.status(404).json("User not found");
+            }else {
+                if(user.followers.includes(req.body.userId)){
+                    await user.updateOne({$pull:{followers:req.body.userId}});
+                    await currentUser.updateOne({$pull:{following:req.params.id}});
+                    res.status(200).json(` unfollowed ${user.username}`);
+                }else {
+                    res.status(403).json("You don't follow this user !");
+                }
+        }}catch(err){
+            res.status(500).json(err);
+        }
+    }else {
+        res.status(403).json("You cannot unfollow yourself !!");
+    }
+})
+
 
 
 
